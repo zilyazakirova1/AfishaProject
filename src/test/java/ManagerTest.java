6e66a237b69ccd5ffe3aeb67d7cac406b09a524b
Neto test <zilya.zakirova1991@gmail.com>
2023-04-27 20:57:45 +0300

    Last changes

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -9,13 +9,27 @@ public class ManagerTest {
         manager.add("Movie 1");
         manager.add("Movie 2");
         manager.add("Movie 3");
+        manager.add("Movie 4");
+        manager.add("Movie 5");
         String[] actual = manager.findAll();
-        String[] expected = {"Movie 1", "Movie 2", "Movie 3"};
+        String[] expected = {"Movie 1", "Movie 2", "Movie 3", "Movie 4", "Movie 5"};
         Assertions.assertArrayEquals(expected, actual);
 
         String[] actual1 = manager.findLast();
-        String[] expected1 = {"Movie 3", "Movie 2", "Movie 1"};
+        String[] expected1 = {"Movie 5", "Movie 4", "Movie 3", "Movie 2", "Movie 1"};
         Assertions.assertArrayEquals(expected1, actual1);
+
+        AfishaManager manager2 = new AfishaManager(4);
+        manager2.add("Movie 10");
+        manager2.add("Movie 20");
+        manager2.add("Movie 30");
+        String[] actual2 = manager2.findAll();
+        String[] expected2 = {"Movie 10", "Movie 20", "Movie 30"};
+        Assertions.assertArrayEquals(expected2, actual2);
+
+        String[] actual3 = manager2.findLast();
+        String[] expected3 = {"Movie 30", "Movie 20", "Movie 10"};
+        Assertions.assertArrayEquals(expected3, actual3);
     }
 
 
